723010dc97c3814271b882341b5538d9982b0726
JuanHanelGL <juan.hanel@colorado.edu>
2023-10-29 23:20:27 +0000

    added PDB90 bash script and unit test

---
+++ run/PDB90.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#set -e # stop on error
+set -u # raise error if variable is unset
+set -o pipefail # fail if any prior step failed
+
+python ../src/PDB90.py --output_folder "../Datasets/all_PDB" --csv_file "../data/somePDB.csv"
