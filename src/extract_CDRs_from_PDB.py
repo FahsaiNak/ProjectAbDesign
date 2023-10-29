d482c02b480ccc4a32b97c026daf6faff0685122
davidgreenblott <david.greenblott@colorado.edu>
2023-10-29 16:27:36 -0600

    pycodestyle extract_CDRs.py

--- src/extract_CDRs_from_PDB.py
+++ src/extract_CDRs_from_PDB.py
@@ -4,22 +4,22 @@ from Bio.PDB import PDBParser
 import processing_utils
 import os
 
-#GLOBAL VARIABLES
+# GLOBAL VARIABLES
 
 #  3 and 1 letter amino acid codes
-d3to1= {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
-            'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
-            'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
-            'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
+d3to1 = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+         'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
+         'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
+         'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
 
 #  Chothia numbering
-CDR_Hpos = {'H1': [26,32], 'H2': [52,56], 'H3': [95,102]}
-CDR_Lpos = {'L1': [24,34], 'L2': [50,56], 'L3': [89,97]}
+CDR_Hpos = {'H1': [26, 32], 'H2': [52, 56], 'H3': [95, 102]}
+CDR_Lpos = {'L1': [24, 34], 'L2': [50, 56], 'L3': [89, 97]}
 
 # Chain Letters for heavy and light chain
-CHAIN_LETTERS = ['H','L']
+CHAIN_LETTERS = ['H', 'L']
 
-#max CDR-sequence length = 20
+# max CDR-sequence length = 20
 # set to minimize compute ime
 MAX_CDR_LENGTH = 20
 
@@ -28,36 +28,42 @@ MAX_CDR_LENGTH = 20
 ELEMENT_TO_CHECK = 'N'
 
 # file paths for loading raw pdb files and saving CDR pdb files
-CHOTHIA_PDB_FILE_PATH = '../Datasets/chothia_pdb_files' 
+CHOTHIA_PDB_FILE_PATH = '../Datasets/chothia_pdb_files'
 CDR_FILE_PATH = '../Datasets/CDR_pdb_files'
 
+
 def get_letter_to_extract(head_df, query_value):
-    '''Given the head data frame from a pdb file, return the letter corresponding
-        to the chain of interest
+    '''Given the head data frame from a pdb file,
+       return the letter corresponding
+       to the chain of interest
         ----------
         head_df: DataFrame
-                  a data frame information on what letters are used to represent
+                  a data frame information on what
+                  letters are used to represent
                   chans in the pdb file atom_df
         query_value: str
-                     the letter corresponding to the chain we would like to extract
+                     the letter corresponding to the
+                     chain we would like to extract
         Returns
         -------
-        letter_to_extract: str
-                           the letter by which the heavy or light chain is labeled
+        let_extract: str
+                           the letter by which the heavy
+                           or light chain is labeled
     '''
 
-    
     head_df.drop_duplicates(subset=['entry'], inplace=True)
-    chain_indices = head_df["entry"].str.find(query_value+'CHAIN=')+len(query_value+'CHAIN=')
+    chain_indices = head_df["entry"].str.find(query_value+'CHAIN=') + \
+        len(query_value+'CHAIN=')
     first_index = (chain_indices > len(query_value+'CHAIN=')).idxmax()
     if first_index == 0:
         return None
     first_value = chain_indices[first_index]
-    letter_to_extract = head_df['entry'].iloc[first_index][first_value:first_value+1]
+    let_extract = head_df['entry'].iloc[first_index][first_value:first_value+1]
 
-    return letter_to_extract
+    return let_extract
 
-#TODO named parameters for all functions
+
+# TODO named parameters for all functions
 def extract_CDR(chain_df, CDR_bounds):
     '''Extracts atoms, residues, and positions from data frame
         containing the chain of interest for specific CDR residue numbers
@@ -74,32 +80,34 @@ def extract_CDR(chain_df, CDR_bounds):
         Returns
         -------
         CDR_df: DataFrame
-                a data frame containing only the rows in chain_df specified by the
-                CDR_bounds
+                a data frame containing only the rows in chain_df specified
+                by the CDR_bounds
     '''
-    
+
     start_residue = CDR_bounds[0]
     end_residue = CDR_bounds[1]
 
-    start_residue_df = chain_df[chain_df.residue_number==start_residue]
-    if len(start_residue_df)==0:
+    start_residue_df = chain_df[chain_df.residue_number == start_residue]
+    if len(start_residue_df) == 0:
         print(f'Incomplete CDR chain in file')
         return None
     start_residue_atom_idx = start_residue_df.index.values[0]
-    end_residue_df = chain_df[chain_df.residue_number==end_residue]
-    if len(end_residue_df)==0:
+    end_residue_df = chain_df[chain_df.residue_number == end_residue]
+    if len(end_residue_df) == 0:
         print(f'Incomplete CDR chain in file')
         return None
     end_residue_atom_idx = end_residue_df.index.values[-1]
 
-    CDR_df = chain_df.iloc[start_residue_atom_idx:end_residue_atom_idx+1].reset_index(drop=True)
+    CDR_df = chain_df.iloc[start_residue_atom_idx:end_residue_atom_idx+1]
+    CDR_df = CDR_df.reset_index(drop=True)
     return CDR_df
+
 
 def get_CDR_length(pdb_id, CDR_save_name):
     '''Returns the number of residues contained within a pdb file of a CDR
         ----------
         pdb_id: str
-                corresponds to the pdb file we will load in 
+                corresponds to the pdb file we will load in
         CDR_save_name: str
                        the path and file for the pdb entry to read in
         Returns
@@ -114,15 +122,16 @@ def get_CDR_length(pdb_id, CDR_save_name):
         for model in structure:
 
             for chain in model:
-                seq= []
+                seq = []
                 for residue in chain:
                     seq.append(d3to1[residue.resname])
-                CDR_seq=''.join(seq)
-        CDR_length = len(CDR_seq)    
+                CDR_seq = ''.join(seq)
+        CDR_length = len(CDR_seq)
         return CDR_length
-    except Exception: #TODO fix this try except to make more specific
+    except Exception:  # TODO fix this try except to make more specific
         print(f'PDB Const Error for file: {CDR_save_name}')
         return None
+
 
 def save_CDR(CDR_df, pdb_id, CDR_type):
     '''Merges newly extracted atomistic CDR data frame with original pdb file
@@ -131,19 +140,22 @@ def save_CDR(CDR_df, pdb_id, CDR_type):
         CDR_df: DataFrame
                 the atomistic CDR data frame to save
         pdb_id: str
-                corresponds to the pdb file from which the CDR_df was extracted  
+                corresponds to the pdb file from which the CDR_df was extracted
         CDR_type: str
                   the CDR type that this CDR data frame is
     '''
 
-    CDR_save_name = CDR_FILE_PATH +'/' +pdb_id+'_'+CDR_type+'.pdb' 
+    CDR_save_name = CDR_FILE_PATH + '/' + pdb_id + '_' + CDR_type + '.pdb'
 
     pdb_to_save = PandasPdb().read_pdb(CHOTHIA_PDB_FILE_PATH+'/'+pdb_id+'.pdb')
-    pdb_to_save.df['ATOM'] = pd.merge(pdb_to_save.df['ATOM'], CDR_df, how='inner')
+    pdb_to_save.df['ATOM'] = pd.merge(pdb_to_save.df['ATOM'],
+                                      CDR_df,
+                                      how='inner')
     pdb_to_save.to_pdb(path=CDR_save_name,
-                            records=['ATOM', 'OTHERS', 'ANISOU'],
-                            gz=False,
-                            append_newline=True)
+                       records=['ATOM', 'OTHERS', 'ANISOU'],
+                       gz=False,
+                       append_newline=True)
+
 
 def main():
 
@@ -152,20 +164,20 @@ def main():
 
     files = os.listdir(CHOTHIA_PDB_FILE_PATH)
 
-    for index, file in enumerate (files):
-        
+    for index, file in enumerate(files):
+
         pdb_id = file[-8:-4]
         print('Index:', index, pdb_id)
-        pdb_df = PandasPdb().read_pdb(CHOTHIA_PDB_FILE_PATH +'/' + file) 
-        
+        pdb_df = PandasPdb().read_pdb(CHOTHIA_PDB_FILE_PATH + '/' + file)
+
         head_df = pdb_df.df['OTHERS']
-        atom_df = pdb_df.df ['ATOM']
-        
+        atom_df = pdb_df.df['ATOM']
+
         atom_name_series = atom_df['atom_name']
 
-        #removes pdb files that do not contain full atom list
-        #by checking for presence of nitrogen atom
-        #assumes that if N is present then it's full chain
+        # removes pdb files that do not contain full atom list
+        # by checking for presence of nitrogen atom
+        # assumes that if N is present then it's full chain
         if ELEMENT_TO_CHECK in atom_name_series.values:
 
             for chain_letter in CHAIN_LETTERS:
@@ -176,15 +188,16 @@ def main():
                     print(f'No H chain in file')
                 else:
 
-                    chain_df = atom_df[atom_df.chain_id == chain_pdb_letter].reset_index(drop=True)
-                    
+                    chain_df = atom_df[atom_df.chain_id == chain_pdb_letter]
+                    chain_df = chain_df.reset_index(drop=True)
                     for CDR_type, CDR_bounds in CDR_Hpos.items():
-                        
+
                         CDR_df = extract_CDR(chain_df, CDR_bounds)
-                        
+
                         save_CDR(CDR_df, pdb_id, CDR_type)
 
-                        CDR_save_name = CDR_FILE_PATH +'/' +pdb_id+'_'+CDR_type+'.pdb' 
+                        CDR_save_name = CDR_FILE_PATH + '/' + pdb_id \
+                            + '_' + CDR_type + '.pdb'
                         CDR_length = get_CDR_length(pdb_id, CDR_save_name)
 
                         if CDR_length is None:
@@ -196,6 +209,7 @@ def main():
             print(f"{ELEMENT_TO_CHECK} is not in the DataFrame.")
             os.remove(CHOTHIA_PDB_FILE_PATH + '/' + file)
         print('----------------------------------------------')
+
 
 if __name__ == '__main__':
     main()
